b7de77daedca8f5c8e0b79300942787a0c287334
s0cketT <137925156+s0cketT@users.noreply.github.com>
2025-03-21 18:39:50 +0300

    Branch creation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.tasklist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tasklist"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
